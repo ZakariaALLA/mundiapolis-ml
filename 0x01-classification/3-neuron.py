e64e84a3a41185e89cf1e389425c867dc2de659b
Zakria ALLA <z.alla@mundiapolis.ma>
2021-03-26 16:18:12 +0100

    add task 3

---
+++ 0x01-classification/3-neuron.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+class Neuron:
+
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return (self.__W)
+
+    @property
+    def b(self):
+        return (self.__b)
+
+    @property
+    def A(self):
+        return (self.__A)
+
+    def forward_prop(self, X):
+        z = np.matmul(self.W, X) + self.b
+        self.__A = 1 / (1 + (np.exp(-z)))
+        return (self.A)
+
+    def cost(self, Y, A):
+        m = Y.shape[1]
+        m_loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
+        cost = (1 / m) * (-(m_loss))
+        return (cost)
