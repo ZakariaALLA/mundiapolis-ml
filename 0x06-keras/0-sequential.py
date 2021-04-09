4ad00dd40aab33330a1b555ea98e8b8c5b600bbe
Zakria ALLA <z.alla@mundiapolis.ma>
2021-04-09 21:59:59 +0100

    input

--- 0x06-keras/0-sequential.py
+++ 0x06-keras/0-sequential.py
@@ -16,14 +16,9 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     reg_l2 = K.regularizers.l2(lambtha)
     for i in range(len(layers)):
         if i is 0:
-            sequential.append(K.layers.Dense(layers[i],
-                                             activation=activations[i],
-                                             kernel_regularizer=reg_l2,
-                                             input_shape=shape))
+            sequential.append(K.layers.Dense(layers[i], activation=activations[i], kernel_regularizer=reg_l2, input_shape=shape))
         else:
             sequential.append(K.layers.Dropout(1 - keep_prob))
-            sequential.append(K.layers.Dense(layers[i],
-                                             activation=activations[i],
-                                             kernel_regularizer=reg_l2))
+            sequential.append(K.layers.Dense(layers[i], activation=activations[i], kernel_regularizer=reg_l2))
     model = K.Sequential(sequential)
     return model
