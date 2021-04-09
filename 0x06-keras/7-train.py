52b75d4c8ea5c2afe21ebe8e889ee9c9c9e533b7
Zakria ALLA <z.alla@mundiapolis.ma>
2021-04-09 23:43:43 +0100

    task 7

---
+++ 0x06-keras/7-train.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Train the model and learning rate
+"""
+
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, validation_data=None, early_stopping=False,
+                patience=0, learning_rate_decay=False, alpha=0.1, decay_rate=1, verbose=True, shuffle=False):
+    """
+    The train_model function
+    """
+    callback = []
+
+    if early_stopping and validation_data:
+        callback.append(K.callbacks.EarlyStopping(monitor='loss', patience=patience))
+
+    def learning_rate(epoch):
+        """ 
+        The learning_rate function
+        """
+        return (alpha / (1 + decay_rate * epoch))
+
+    if learning_rate_decay and validation_data:
+        callback.append(K.callbacks.LearningRateScheduler(learning_rate, verbose=1))
+
+    if callback == []:
+        callback = None
+
+    history = network.fit(x=data, y=labels, batch_size=batch_size, epochs=epochs, validation_data=validation_data,
+                          callbacks=callback, verbose=verbose, shuffle=shuffle)
+    return history
