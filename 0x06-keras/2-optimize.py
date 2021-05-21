a12d1dc7ed548b554fde37d0ac932366d159ab50
ZakariaALLA <64694853+ZakariaALLA@users.noreply.github.com>
2021-05-21 10:35:23 +0100

    Update 2-optimize.py

--- 0x06-keras/2-optimize.py
+++ 0x06-keras/2-optimize.py
@@ -9,7 +9,7 @@ import tensorflow.keras as K
 
 def optimize_model(network, alpha, beta1, beta2):
     """
-    Optimize model
+    Defines optimize model
     """
     network.compile(optimizer=K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2), loss='categorical_crossentropy', metrics=['accuracy'])
     return None
